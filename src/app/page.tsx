8b7ac06142adae0fd750cea882f5171ec92cedad
Emmanuel Uwem <emmanueluwem560@gmail.com>
2025-07-04 15:03:38 +0100

    fix: adjust layout of Home component for better responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import MintHero from "@/components/mintHero";
 export default function Home() {
   return (
-    <div className="flex w-full h-full items-center justify-start flex-col py-20 px-4">
+    <div className="flex w-full h-full items-center justify-start flex-col py-20 px-4 max-md:pb-20">
       <MintHero />
     </div>
   );
